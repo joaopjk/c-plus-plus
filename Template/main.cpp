9d1025e37ff0e1fea639f149b5ee248ea5ca9551
joaopjk <joaopjk48@gmail.com>
2022-08-24 21:55:36 -0300

    Variables

--- Template/main.cpp
+++ Template/main.cpp
@@ -5,3 +5,4 @@ int main()
   auto result = (10 <=> 20) > 0;
   std::cout << result << std::endl;
 }
+1:43:10
--- VairiablesAndContants/main.cpp
+++ VairiablesAndContants/main.cpp
@@ -6,14 +6,16 @@ int main()
   // int age {21};
 
   // cout << age << endl;
-  cout << "Enter the width of the room: ";
-  int room_width{0};
-  cin >> room_width;
 
-  cout << "Enter the lenght of the room: ";
-  int room_lenght{0};
-  cin >> room_lenght;
+  // cout << "Enter the width of the room: ";
+  // int room_width{0};
+  // cin >> room_width;
 
-  cout << "The area of the room is " << room_width * room_lenght << " square feet" << endl;
+  // cout << "Enter the lenght of the room: ";
+  // int room_lenght{0};
+  // cin >> room_lenght;
+
+  // cout << "The area of the room is " << room_width * room_lenght << " square feet" << endl;
+  
   return 0;
 }
