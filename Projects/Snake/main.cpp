92e3a841be120e2af97df146468a8990d77866f6
joaopjk <joaopjk48@gmail.com>
2022-08-27 19:53:32 -0300

    Snake part 3

--- Projects/Snake/main.cpp
+++ Projects/Snake/main.cpp
@@ -7,6 +7,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection
 {
   STOP = 0,
@@ -49,6 +51,20 @@ void Draw()
         cout << "O";
       else if (i == fruitY && j == fruitX)
         cout << "F";
+      else
+      {
+        bool print = false;
+        for (int k = 0; k < nTail; k++)
+        {
+          if (tailX[k] == j && tailY[k] == i)
+          {
+            cout << "o";
+            print = true;
+          }
+        }
+        if (!print)
+          cout << " ";
+      }
 
       if (j == width - 1)
         cout << "#";
@@ -89,6 +105,22 @@ void Input()
 
 void Logic()
 {
+  int prevX = tailX[0];
+  int prevY = tailY[0];
+  int prev2X, prev2Y;
+
+  for (int i = 1; i < nTail; i++)
+  {
+    tailX[0] = static_cast<int>(x);
+    tailY[0] = static_cast<int>(y);
+    prev2X = tailX[i];
+    prev2Y = tailY[i];
+    tailX[i] = prevX;
+    tailY[i] = prevY;
+    prevX = prev2X;
+    prevY = prev2Y;
+  }
+
   switch (dir)
   {
   case LEFT:
@@ -108,19 +140,24 @@ void Logic()
   if (x > width || x < 0 || y > height || y < 0)
     gameOver = true;
   if (x == fruitX && y == fruitY)
+  {
+    nTail++;
     score += 10;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+  }
 }
 
 int main()
 {
   Setup();
+
   while (!gameOver)
   {
     Draw();
     Input();
     Logic();
-    Sleep(100);
-    // Sleep(10);
+    Sleep(50);
   }
 
   return 0;
