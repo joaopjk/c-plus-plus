4fd45f5e68542d147084e882db8ea2a8dc429d67
joaopjk <joaopjk48@gmail.com>
2022-08-27 15:53:16 -0300

    Snake part 1

---
+++ basics/TipicsInCPP/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Where your live? " << std::endl;
+  std::string country;
+  std::cin >> country;
+  std::cout << "I've heard great things about " << country << ". I'd like to go somentime" << std::endl;
+
+  return 0;
+}
