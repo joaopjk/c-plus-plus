78de582ef2a129682bfd0160de71ac906be9a063
joaopjk <joaopjk48@gmail.com>
2022-08-27 19:55:44 -0300

    checkpoint

--- Projects/Snake/main.cpp
+++ Projects/Snake/main.cpp
@@ -2,14 +2,13 @@
 #include <conio.h>
 #include <windows.h>
 using namespace std;
-
 bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100];
 int nTail;
-enum eDirection
+enum eDirecton
 {
   STOP = 0,
   LEFT,
@@ -17,8 +16,7 @@ enum eDirection
   UP,
   DOWN
 };
-eDirection dir;
-
+eDirecton dir;
 void Setup()
 {
   gameOver = false;
@@ -26,15 +24,13 @@ void Setup()
   x = width / 2;
   y = height / 2;
   fruitX = rand() % width;
-  fruitY = rand() & height;
+  fruitY = rand() % height;
   score = 0;
 }
-
 void Draw()
 {
-  system("cls");
-
-  for (int i = 0; i < width + 1; i++)
+  system("cls");a
+  for (int i = 0; i < width + 2; i++)
     cout << "#";
   cout << endl;
 
@@ -44,9 +40,6 @@ void Draw()
     {
       if (j == 0)
         cout << "#";
-      else
-        cout << " ";
-
       if (i == y && j == x)
         cout << "O";
       else if (i == fruitY && j == fruitX)
@@ -72,15 +65,14 @@ void Draw()
     cout << endl;
   }
 
-  for (int i = 0; i < width + 1; i++)
+  for (int i = 0; i < width + 2; i++)
     cout << "#";
   cout << endl;
-  cout << "Score: " << score << endl;
+  cout << "Score:" << score << endl;
 }
-
 void Input()
 {
-  if (_kbhit()) // Return boolean, use to know inputs is pressing in keyboard
+  if (_kbhit())
   {
     switch (_getch())
     {
@@ -90,11 +82,11 @@ void Input()
     case 'd':
       dir = RIGHT;
       break;
-    case 's':
-      dir = DOWN;
-      break;
     case 'w':
       dir = UP;
+      break;
+    case 's':
+      dir = DOWN;
       break;
     case 'x':
       gameOver = true;
@@ -102,17 +94,15 @@ void Input()
     }
   }
 }
-
 void Logic()
 {
   int prevX = tailX[0];
   int prevY = tailY[0];
   int prev2X, prev2Y;
-
+  tailX[0] = x;
+  tailY[0] = y;
   for (int i = 1; i < nTail; i++)
   {
-    tailX[0] = static_cast<int>(x);
-    tailY[0] = static_cast<int>(y);
     prev2X = tailX[i];
     prev2Y = tailY[i];
     tailX[i] = prevX;
@@ -120,7 +110,6 @@ void Logic()
     prevX = prev2X;
     prevY = prev2Y;
   }
-
   switch (dir)
   {
   case LEFT:
@@ -135,30 +124,39 @@ void Logic()
   case DOWN:
     y++;
     break;
+  default:
+    break;
   }
+  if (x >= width)
+    x = 0;
+  else if (x < 0)
+    x = width - 1;
+  if (y >= height)
+    y = 0;
+  else if (y < 0)
+    y = height - 1;
 
-  if (x > width || x < 0 || y > height || y < 0)
-    gameOver = true;
+  for (int i = 0; i < nTail; i++)
+    if (tailX[i] == x && tailY[i] == y)
+      gameOver = true;
+
   if (x == fruitX && y == fruitY)
   {
-    nTail++;
     score += 10;
     fruitX = rand() % width;
     fruitY = rand() % height;
+    nTail++;
   }
 }
-
 int main()
 {
   Setup();
-
   while (!gameOver)
   {
     Draw();
     Input();
     Logic();
-    Sleep(50);
+    Sleep(10); // sleep(10);
   }
-
   return 0;
 }
