203b46dedd89a241d2db7c9a646d96df21e7f458
joaopjk <joaopjk48@gmail.com>
2022-08-28 21:43:58 -0300

    Comparing stuff

---
+++ basics/ComparingStuff/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number1{45};
+  int number2{60};
+
+  cout << boolalpha;
+
+  cout << (number1 < number2) << endl;
+  cout << (number1 <= number2) << endl;
+  cout << (number1 >number2) << endl;
+  cout << (number1 >= number2) << endl;
+  cout << (number1 == number2) << endl;
+  cout << (number1 != number2) << endl;
+
+  return 0;
+}
