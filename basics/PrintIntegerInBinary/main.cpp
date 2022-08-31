dded2875916d3ae7c13d19c0edc87bf3e0662d41
joaopjk <joaopjk48@gmail.com>
2022-08-31 19:48:43 -0300

    Shift operators

--- basics/PrintIntegerInBinary/main.cpp
+++ basics/PrintIntegerInBinary/main.cpp
@@ -5,9 +5,9 @@ int main()
 {
   unsigned short int data{0b111111110};
 
-  std::cout << std::dec << data << std::endl;
-  std::cout << std::hex << data << std::endl;
-  std::cout << std::bitset<16>(data) << std::endl;
+  std::cout << std::showbase << std::dec << data << std::endl;
+  std::cout << std::showbase << std::hex << data << std::endl;
+  std::cout << std::showbase << std::bitset<16>(data) << std::endl;
 
   return 0;
 }
---
+++ basics/ShiftOperators/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bitset>
+
+int main()
+{
+  unsigned short int value{0xff0u};
+
+  std::cout << std::bitset<16>(value) << value << std::endl;
+
+  std::cout << std::endl;
+  std::cout << "Shifting right" << std::endl;
+
+  // shift right 1 bit position
+  value = static_cast<unsigned short int>(value >> 1);
+  std::cout << std::bitset<16>(value) << " " << value << "[After shift 1 bit position right]"
+            << std::endl;
+
+  value = static_cast<unsigned short int>(value << 1);
+  std::cout << std::bitset<16>(value) << " " << value << "[After shift 1 bit position left]"
+            << std::endl;
+
+  /*
+  Shifting right divides by 2>n
+  Shifting left multiplies by 2>n
+  This rule breaks if you throw off 1's either to the right or the left
+  */
+
+  std::cout << (value >> 1) << std::endl;
+
+  return 0;
+}
