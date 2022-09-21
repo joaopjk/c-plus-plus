ac636e8fd78df94b100f8e0032251f42ded801a8
joaopjk <joaopjk48@gmail.com>
2022-09-21 14:00:13 -0300

    While loop

---
+++ Teste/src/Teste.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Teste.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
